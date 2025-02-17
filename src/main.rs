cb1504396c4b1724848cfdee4a61bb4a6d1889f9
Alex Yun <iamalexyundev@gmail.com>
2025-02-17 22:32:48 +0900

    initial commit

---
+++ src/main.rs
@@ -0,0 +1,28 @@
+use std::io;
+fn main() {
+    let mut index = String::new();
+    println!("Enter n-th element of sequence:");
+    let index: i32 = loop {
+        io::stdin()
+            .read_line(&mut index)
+            .expect("Failed to get input");
+        match index.trim().parse() {
+            Ok(idx) => break idx,
+            Err(_) => {
+                println!("Please enter a number!");
+                continue;
+            }
+        };
+    };
+    let number = find_fib(index);
+    println!("Fibonacci number is {number}");
+}
+
+fn find_fib(index: i32) -> i64 {
+    println!("{}", index);
+    if index == 0 {
+        return 0;
+    } else {
+        return find_fib(index - 1);
+    }
+}
